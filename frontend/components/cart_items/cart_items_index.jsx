b920511d0682da2bc09bbffa92d680ef1983f3ae
rordev43 <angelfeliciano206@gmail.com>
2017-09-25 16:27:53 -0700

    Add cart header and styling

--- frontend/components/cart_items/cart_items_index.jsx
+++ frontend/components/cart_items/cart_items_index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import CartItemsIndexItem from './cart_items_index_item';
+import { Link } from 'react-router-dom';
 
 export default class CartItemsIndex extends React.Component {
   constructor(props) {
@@ -12,18 +13,22 @@ export default class CartItemsIndex extends React.Component {
 
   render() {
     const { deleteCartItem, cartProducts } = this.props;
+    const products = cartProducts.map( cartProduct =>
+      <CartItemsIndexItem
+      key={cartProduct.id}
+      deleteCartItem={deleteCartItem}
+      cartProduct={cartProduct}/>);
+
     return (
       <div className="cart-body">
         <div className="cart-header">
           <div className="cart-msg">{cartProducts.length} items in your cart</div>
-
+          <Link
+            to="/products"
+            className="keep-shopping-btn">Keep shopping</Link>
         </div>
         <div className="cart-items">
-        {cartProducts.map( cartProduct =>
-          <CartItemsIndexItem
-          key={cartProduct.id}
-          deleteCartItem={deleteCartItem}
-          cartProduct={cartProduct}/>)}
+        {products}
         </div>
       </div>
     );
