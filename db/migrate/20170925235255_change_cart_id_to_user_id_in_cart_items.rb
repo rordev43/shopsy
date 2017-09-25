584e5114953ba3d6fcf052f14c5924dee2874a58
rordev43 <angelfeliciano206@gmail.com>
2017-09-25 16:54:32 -0700

    Reset database

--- db/migrate/20170925235255_change_cart_id_to_user_id_in_cart_items.rb
+++ db/migrate/20170925235255_change_cart_id_to_user_id_in_cart_items.rb
@@ -1,6 +1,5 @@
-class DropCartTable < ActiveRecord::Migration[5.1]
+class ChangeCartIdToUserIdInCartItems < ActiveRecord::Migration[5.1]
   def change
-    drop_table :carts
     remove_column :cart_items, :cart_id
     add_column :cart_items, :user_id, :integer, null: false
   end
