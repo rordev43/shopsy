aee949101dfcf172449b715f315fa04375d4dbf6
rordev43 <angelfeliciano206@gmail.com>
2017-09-25 20:05:27 -0700

    Style cart page

--- frontend/components/cart_items/cart_items_index_item.jsx
+++ frontend/components/cart_items/cart_items_index_item.jsx
@@ -17,10 +17,16 @@ export default class CartItemsIndexItem extends React.Component {
     return (
       <div className="cart-product">
         <img className="cart-product-img" src={cartProduct.image_url}/>
-        <ul>
+        <ul className="cart-product-details">
           <li>{cartProduct.title}</li>
-          <button onClick={this.handleClick}>Remove Item</button>
+          <li>Seller: {cartProduct.seller.username}</li>
+          <li>{cartProduct.description}</li>
         </ul>
+        <button
+          className="delete-btn"
+          onClick={this.handleClick}>
+          <img src="http://res.cloudinary.com/brainzilla/image/upload/v1506393086/close_button_gwuq4f.png"/>
+          </button>
       </div>
     );
   }
