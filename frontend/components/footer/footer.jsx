6a951c63a98a2ab81442a44ec389f4a0be072010
rordev43 <angelfeliciano206@gmail.com>
2017-09-27 16:46:42 -0700

    Style footer

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
-export default class Footer extends React.Component {
-  render() {
-    return <div className="footer">Footer</div>;
-  }
-}
+const Footer = () => (
+  <div className="footer">
+    <div className="name">Alexander Happ</div>
+    <ul className="footer-links">
+      <li>
+        <a href={"https://github.com/brainzilla79"}>
+          <img src="http://res.cloudinary.com/brainzilla/image/upload/v1506554707/GitHub-Mark-64px_kcj6xx.png" />
+        </a>
+      </li>
+      <li>
+        <a href={"https://www.linkedin.com/in/alexander-happ-16a33b133/"}>
+          <img src="http://res.cloudinary.com/brainzilla/image/upload/v1506554703/In-2C-66px-TM_dofc6h.png" />
+        </a>
+      </li>
+    </ul>
+  </div>
+);
+
+export default Footer;
