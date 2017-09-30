6285e5baaf19e7074750bbd62b97fb025f9ed62d
rordev43 <angelfeliciano206@gmail.com>
2017-09-30 09:09:24 -0700

    Fix banner text

--- frontend/components/modals/auth_modal_container.jsx
+++ frontend/components/modals/auth_modal_container.jsx
@@ -1,13 +1,9 @@
-import { connect } from 'react-redux';
-import AuthModal from './auth_modal';
+import { connect } from "react-redux";
+import AuthModal from "./auth_modal";
 
 const mapStateToProps = (state, ownProps) => {
-  const formType = ownProps.match.path === '/login' ? "Login" : "Signup";
+  const formType = ownProps.match.path === "/login" ? "Login" : "Signup";
   return { formType };
 };
 
-
-export default connect(
-  mapStateToProps,
-  null
-)(AuthModal);
+export default connect(mapStateToProps, null)(AuthModal);
