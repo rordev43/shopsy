ccfff4760dc7c5966d88d64dce21f893783ee72a
rordev43 <angelfeliciano206@gmail.com>
2017-10-19 11:00:01 -0700

    Add cart count

--- frontend/components/greeting/greeting.jsx
+++ frontend/components/greeting/greeting.jsx
@@ -2,50 +2,73 @@ import React from "react";
 import { Link } from "react-router-dom";
 import SearchFormContainer from "../search/search_form_container";
 
-const sessionLinks = login => {
-  const guestUser = { username: "Guest", password: "123456" };
-  const loginDummy = () => login(guestUser);
-  return (
-    <nav className="session-nav">
-      <ul className="session-btns">
-        <li>
-          <Link to={`/signup`}>Register</Link>
-        </li>
-        <li>
-          <Link to={`/login`}>Sign In</Link>
-        </li>
-        <li>
-          <button onClick={loginDummy}>Demo</button>
-        </li>
-      </ul>
-    </nav>
-  );
-};
+class Greeting extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
-const welcomeGreeting = (currentUser, logout) => {
-  return (
-    <div>
+  componentDidMount() {
+    this.props.getCartItems();
+
+  }
+
+  sessionLinks() {
+    const guestUser = { username: "Guest", password: "123456" };
+    const loginDummy = () => this.props.login(guestUser);
+    return (
       <nav className="session-nav">
         <ul className="session-btns">
-          <li className="welcome-msg">Welcome, {currentUser.username}</li>
           <li>
-            <button onClick={logout}>Logout</button>
+            <Link to={`/signup`}>Register</Link>
           </li>
           <li>
-            <Link to={`/cart`}>
-              <img
-                className="cart-icon"
-                src="https://res.cloudinary.com/brainzilla/image/upload/v1506036925/cart_sisp9z.png"
-              />
-            </Link>
+            <Link to={`/login`}>Sign In</Link>
+          </li>
+          <li>
+            <button onClick={loginDummy}>Demo</button>
           </li>
         </ul>
       </nav>
-    </div>
-  );
-};
+    );
+  }
 
-const Greeting = ({ currentUser, logout, login }) =>
-  currentUser ? welcomeGreeting(currentUser, logout) : sessionLinks(login);
+  welcomeGreeting(currentUser, logout, cartCount) {
+    let cartNum;
+    if (cartCount > 0) {
+      cartNum = <div className="cart-num">{cartCount}</div>;
+    } else {
+      cartNum = "";
+    }
+    return (
+      <div>
+        <nav className="session-nav">
+          <ul className="session-btns">
+            <li className="welcome-msg">Welcome, {currentUser.username}</li>
+            <li>
+              <button onClick={logout}>Logout</button>
+            </li>
+            <li>
+              <Link to={`/cart`}>
+                <img
+                  className="cart-icon"
+                  src="https://res.cloudinary.com/brainzilla/image/upload/v1506036925/cart_sisp9z.png"
+                />
+              </Link>
+             {cartNum}
+            </li>
+          </ul>
+        </nav>
+      </div>
+    );
+  }
+
+  render() {
+    if (this.props.currentUser) {
+      return this.welcomeGreeting(this.props.currentUser, this.props.logout, this.props.cartCount);
+    } else {
+      return this.sessionLinks(this.props.login);
+    }
+  }
+}
 
 export default Greeting;
--- frontend/components/greeting/greeting_container.jsx
+++ frontend/components/greeting/greeting_container.jsx
@@ -1,12 +1,15 @@
 import { connect } from 'react-redux';
 import { logout, login } from '../../actions/session_actions';
+import { getCartItems } from '../../actions/cart_items_actions';
 import Greeting from './greeting';
 
 const mapStateToProps = ({cartItems, session}) => ({
+  cartCount: Object.keys(cartItems).length,
   currentUser: session.currentUser
 });
 
 const mapDispatchToProps = dispatch => ({
+  getCartItems: () => dispatch(getCartItems()),
   logout: () => dispatch(logout()),
   login: (user) => dispatch(login(user))
 });
