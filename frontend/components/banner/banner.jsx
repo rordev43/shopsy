818d39944844d9c18ce3b9a0ad73b4b342f01c49
rordev43 <angelfeliciano206@gmail.com>
2017-11-23 12:02:08 -0800

    Fix banner link

--- frontend/components/banner/banner.jsx
+++ frontend/components/banner/banner.jsx
@@ -5,7 +5,7 @@ const Banner = () => (
   <div className="banner">
     <div className="banner-image">
       <div className="banner-text" />
-      <Link to={"/products"}>
+      <Link to={"/categories/1"}>
         <div className="banner-btn">Shop All Apparel</div>
       </Link>
     </div>
