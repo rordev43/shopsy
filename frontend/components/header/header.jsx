6eca8a67c702295242cbe8f6da88b9a00e39f0f2
rordev43 <angelfeliciano206@gmail.com>
2017-11-24 10:54:39 -0800

    Remove react responsive pkg

--- frontend/components/header/header.jsx
+++ frontend/components/header/header.jsx
@@ -3,7 +3,6 @@ import { Link, withRouter } from "react-router-dom";
 import SearchFormContainer from "../search/search_form_container";
 import GreetingContainer from "../greeting/greeting_container";
 import CategoriesNavContainer from "./categories_nav_container";
-import MediaQuery from "react-responsive";
 
 export default class Header extends React.Component {
   render() {
@@ -12,16 +11,14 @@ export default class Header extends React.Component {
           <Link to="/">
             <h1 className="logo">Shopsy</h1>
           </Link>
-          <MediaQuery query="(min-width: 860px)">
-            <SearchFormContainer />
-          </MediaQuery>
+          <div className="desktop-search-container">
+            <SearchFormContainer id="desktopSearch"/>
+          </div>
           <GreetingContainer />
         </div>
-        <MediaQuery query="(max-width: 860px)">
           <div className="search-sub-header">
-            <SearchFormContainer />
+            <SearchFormContainer id="mobileSearch"/>
           </div>
-        </MediaQuery>
         <div className="sub-header">
           <CategoriesNavContainer />
         </div>
