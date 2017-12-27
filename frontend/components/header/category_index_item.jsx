547a883c4ce9ae9e2cc0762964c838e44c5bd767
rordev43 <angelfeliciano206@gmail.com>
2017-12-27 15:54:43 -0800

    Add click multiselect on product form

--- frontend/components/header/category_index_item.jsx
+++ frontend/components/header/category_index_item.jsx
@@ -1,20 +1,32 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const CategoryIndexItem = ({ category, type }) => {
-  if (type === 'option') {
-    return (
-      <option value={category.id}>{category.name}</option>
-    );
-  } else {
-    return (
-    <li>
-      <Link to={`/categories/${category.id}`}>{category.name}</Link>
-    </li>
-    );
-
+export default class CategoryIndexItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleMouseDown = this.handleMouseDown.bind(this);
   }
-  
-};
 
-export default CategoryIndexItem;
+  handleMouseDown(e) {
+    e.preventDefault();
+    const status = $(e.target).prop("selected");
+    if (status) {
+      $(e.target).prop("selected", false);
+    } else {
+      $(e.target).prop("selected", true);
+    }
+  } 
+
+  render() {
+    if (this.props.type === "option") {
+      return <option onMouseDown={this.handleMouseDown} value={this.props.category.id}>{this.props.category.name}</option>;
+    } else {
+      return <li>
+          <Link to={`/categories/${this.props.category.id}`}>{this.props.category.name}</Link>
+        </li>;
+    }
+  }
+}
+
+
+
